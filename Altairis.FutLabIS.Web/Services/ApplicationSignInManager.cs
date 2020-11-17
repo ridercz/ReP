ec797e8f2eb61a3e2091b99699af3fe5613b14d4
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-11-17 21:03:24 +0100

    Allow sign-in using e-mail address, not only user name.

--- Altairis.FutLabIS.Web/Services/ApplicationSignInManager.cs
+++ Altairis.FutLabIS.Web/Services/ApplicationSignInManager.cs
@@ -14,6 +14,17 @@ namespace Altairis.FutLabIS.Web.Services {
     public class ApplicationSignInManager : SignInManager<ApplicationUser> {
         public ApplicationSignInManager(UserManager<ApplicationUser> userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<ApplicationUser>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<ApplicationUser> confirmation) : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation) { }
 
+        public override async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent, bool lockoutOnFailure) {
+            var result = await base.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure);
+
+            // Allow sign-in using e-mail address, not only user name
+            if (result == SignInResult.Failed && userName.Contains('@')) {
+                var userFoundByEmail = await this.UserManager.FindByEmailAsync(userName);
+                if (userFoundByEmail != null) result = await base.PasswordSignInAsync(userFoundByEmail, password, isPersistent, lockoutOnFailure);
+            }
+            return result;
+        }
+
         public override async Task<bool> CanSignInAsync(ApplicationUser user) {
             if (user is null) throw new System.ArgumentNullException(nameof(user));
 
