bcb876623f8908fa7d244b50193dc2ff7b2af17b
Michal Altair Valášek <github@rider.cz>
2023-04-13 21:52:54 +0200

    Mopre descriptive exception.

--- Altairis.ReP.Web/Pages/My/Reservations.cshtml.cs
+++ Altairis.ReP.Web/Pages/My/Reservations.cshtml.cs
@@ -5,6 +5,7 @@ using Altairis.ValidationToolkit;
 using Microsoft.AspNetCore.Identity;
 
 namespace Altairis.ReP.Web.Pages.My;
+
 public class ReservationsModel : PageModel {
     private readonly RepDbContext dc;
     private readonly IDateProvider dateProvider;
@@ -19,7 +20,7 @@ public class ReservationsModel : PageModel {
         this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         this.hoursProvider = hoursProvider ?? throw new ArgumentNullException(nameof(hoursProvider));
         this.attachmentProcessor = attachmentProcessor;
-        this.options = optionsAccessor?.Value ?? throw new ArgumentException(nameof(optionsAccessor));
+        this.options = optionsAccessor?.Value ?? throw new ArgumentException("Options cannot be null or empty.",nameof(optionsAccessor));
     }
 
     [BindProperty]
