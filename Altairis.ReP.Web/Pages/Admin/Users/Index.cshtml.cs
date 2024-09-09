d775321bf905cc5a3a6d9c71c185ae261c01c193
Michal Altair Valášek <github@rider.cz>
2024-09-09 15:41:39 +0200

    Show number of reservations in user list.

--- Altairis.ReP.Web/Pages/Admin/Users/Index.cshtml.cs
+++ Altairis.ReP.Web/Pages/Admin/Users/Index.cshtml.cs
@@ -6,14 +6,14 @@ public class IndexModel(RepDbContext dc) : PageModel {
 
     public IEnumerable<UserInfo> Users { get; set; } = new List<UserInfo>();
 
-    public record UserInfo(int Id, string UserName, string DisplayName, string Email, string Language, string? PhoneNumber, bool Enabled, bool EmailConfirmed);
+    public record UserInfo(int Id, string UserName, string DisplayName, string Email, string Language, string? PhoneNumber, bool Enabled, bool EmailConfirmed, int Reservations);
 
     // Handlers
 
     public async Task OnGetAsync() {
         var q = from u in dc.Users
                 orderby u.UserName
-                select new UserInfo(u.Id, u.UserName ?? string.Empty, u.DisplayName, u.Email ?? string.Empty, u.Language, u.PhoneNumber, u.Enabled, u.EmailConfirmed);
+                select new UserInfo(u.Id, u.UserName ?? string.Empty, u.DisplayName, u.Email ?? string.Empty, u.Language, u.PhoneNumber, u.Enabled, u.EmailConfirmed, u.Reservations.Count);
         this.Users = await q.ToListAsync();
     }
 
